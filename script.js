24ae55408bb8bf2080c05a4f49e814861dd07556
Anish khare <111656312+Anishkhare3@users.noreply.github.com>
2023-09-05 10:54:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let main = document.querySelector(".main")
+// let cursor = document.querySelector(".cursor")
+
+// main.addEventListener("mousemove", function (details) {
+//     cursor.style.left = details.x + "px"
+//     cursor.style.top = details.y + "px"
+//     cursor.style.bottom = details.z + "px"
+// })
+
+
+
